05957066c12b96f256d02e825baadefedea3cb23
Connor Fuhrman <connorfuhrman@email.arizona.edu>
2022-05-17 14:18:52 -0700

    Added CMake mode from MELPA

---
+++ elpa/cmake-mode-20220322.1258/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20220322.1258" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "a7cd15a61c5708ba50482ee485b4c102146861d0")
